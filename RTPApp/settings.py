4cee81766d3e3a7585edd44b590d132b0d5c37a3
unknown <meer.r.patel@gmail.com>
2022-11-05 15:59:40 -0400

    better

--- RTPApp/settings.py
+++ RTPApp/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-p4cl^+)t3p@ocd+o(zciav2+@b^@ri4^i+nz=7ynx43v*(ulmo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 STATIC_URL = 'static/'
 
