35bfab5b913aff3e061a7a4e075c6222001ea466
unknown <meer.r.patel@gmail.com>
2022-11-05 16:18:16 -0400

    pls

--- RTPApp/settings.py
+++ RTPApp/settings.py
@@ -17,8 +17,6 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-autocrlf = False
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
