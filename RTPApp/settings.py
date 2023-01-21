229fa7f4fd54645a9f4baecea1f699ae77180ca3
Meer Patel <meer.r.patel@gmail.com>
2023-01-21 17:54:00 -0500

    Debug= False

--- RTPApp/settings.py
+++ RTPApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p4cl^+)t3p@ocd+o(zciav2+@b^@ri4^i+nz=7ynx43v*(ulmo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost:5000']
 
