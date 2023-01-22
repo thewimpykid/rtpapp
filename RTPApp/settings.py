a03cc36a60be42384e1ec981a434200af579e75f
Meer Patel <meer.r.patel@gmail.com>
2023-01-21 23:24:38 -0500

    worker

--- RTPApp/settings.py
+++ RTPApp/settings.py
@@ -127,3 +127,5 @@ django_heroku.settings(locals())
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_DOMAIN = '.herokuapp.com'
